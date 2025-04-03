173b254921decc12c08ee84626e732679e942540
cfb3 <cfb3@uw.edu>
2025-04-03 09:21:39 -0700

    Pre Spring 25

--- src/routes/open/hello.ts
+++ src/routes/open/hello.ts
@@ -28,7 +28,9 @@ helloRouter.get('/', (request: Request, response: Response) => {
  *
  * @apiDescription TL;DR Use the HTTP POST methods when creating a new resource on
  * an http server/service. The HTTP POST maps to the (C - create) in the acronym CRUD for the
- * common operations performed on a database.
+ * common operations performed on a database. POST methods are NOT idempotent meaning
+ * the request may cause side-effects. Think: creating a new resource with an auto-incrementing
+ * id.
  *
  * @apiSuccess {String} message the String: "Hello, you sent a POST request"
  */
@@ -43,9 +45,11 @@ helloRouter.post('/', (request: Request, response: Response) => {
  * @apiName PutHello
  * @apiGroup Hello
  *
- * @apiDescription TL;DR Use the HTTP PUT methods when altering/changing aa exsisting resource on
- * an http server/service. The HTTP PUT maps to the (U - update) in the acronym CRUD for the
- * common operations performed on a database.
+ * @apiDescription TL;DR Use the HTTP PUT methods when replacing an existing resource on
+ * an http server/service. The HTTP PUT somewhat maps to the (U - update) in the acronym
+ * CRUD for the common operations performed on a database. PUT methods are idempotent meaning
+ * meaning the intended effect on the server of making a single request is the same
+ * as the effect of making several identical requests.
  *
  * @apiSuccess {String} message the String: "Hello, you sent a PUT request"
  */
@@ -56,11 +60,28 @@ helloRouter.put('/', (request: Request, response: Response) => {
 });
 
 /**
+ * @api {patch} /hello Request a Hello World message
+ * @apiName PatchHello
+ * @apiGroup Hello
+ *
+ * @apiDescription TL;DR Use the HTTP PATCH methods when altering/changing an existing resource on
+ * an http server/service. The HTTP PATCH somewhat maps to the (U - update) in the acronym
+ * CRUD for the common operations performed on a database.
+ *
+ * @apiSuccess {String} message the String: "Hello, you sent a PATCH request"
+ */
+helloRouter.patch('/', (request: Request, response: Response) => {
+    response.send({
+        message: 'Hello, you sent a PATCH request',
+    });
+});
+
+/**
  * @api {delete} /hello Request a Hello World message
  * @apiName DeleteHello
  * @apiGroup Hello
  *
- * @apiDescription TL;DR Use the HTTP DELETE methods when delteing an exsiting resource on
+ * @apiDescription TL;DR Use the HTTP DELETE methods when deleting an existing resource on
  * an http server/service. The HTTP DELETE maps to the (D - delete) in the acronym CRUD for the
  * common operations performed on a database.
  *
